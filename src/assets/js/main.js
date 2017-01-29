594e232f4bb5a6b4d8019262e3621355289b57cb
Gabriel Godoy <hi@gabrielgodoy.com>
2017-01-29 00:02:12 -0200

    Separate barba setup in another module

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,29 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/main.styl';
-import * as Barba from 'barba.js';
+import setupBarba from './setupBarba';
 
 document.addEventListener('DOMContentLoaded', function() {
-  let transitionTime = 500;
-  let FadeTransition = Barba.BaseTransition.extend({
-    // Call start when transition begins
-    start: function() {
-      Promise.all([this.newContainerLoading, this.fadeOut()])
-             .then(this.fadeIn.bind(this));
-    },
-    fadeOut: function() {
-      this.oldContainer.classList.toggle('fade-out');
-      return new Promise(resolve => {
-        window.setTimeout(function() {
-          resolve();
-        }, transitionTime);
-      });
-    },
-    fadeIn: function() {
-      this.newContainer.classList.toggle('fade-in');
-      this.done();
-    }
-  });
-
-  Barba.Pjax.getTransition = () => FadeTransition;
-  Barba.Pjax.start();
+  setupBarba();
 });
---
+++ src/assets/js/setupBarba.js
@@ -0,0 +1,27 @@
+import * as Barba from 'barba.js';
+
+export default function setupBarba() {
+  let transitionTime = 500;
+  let FadeTransition = Barba.BaseTransition.extend({
+    // Call start when transition begins
+    start: function() {
+      Promise.all([this.newContainerLoading, this.fadeOut()])
+             .then(this.fadeIn.bind(this));
+    },
+    fadeOut: function() {
+      this.oldContainer.classList.toggle('fade-out');
+      return new Promise(resolve => {
+        window.setTimeout(function() {
+          resolve();
+        }, transitionTime);
+      });
+    },
+    fadeIn: function() {
+      this.newContainer.classList.toggle('fade-in');
+      this.done();
+    }
+  });
+
+  Barba.Pjax.getTransition = () => FadeTransition;
+  Barba.Pjax.start();
+}
